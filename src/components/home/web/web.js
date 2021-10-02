1121b53ead8f0584f1a0827d3d2117579665e3bc
Lucas Galvao <lucasmelo_galvao@hotmail.com>
2021-10-02 13:25:16 -0300

    finishing

--- src/components/home/web/web.js
+++ src/components/home/web/web.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import './web.css'
+import certificate from './../../../assets/icones/certificate.png'
+import book from './../../../assets/icones/books.png'
+import work from './../../../assets/icones/work.png'
+import skills from './../../../assets/icones/skills.png'
+import project from './../../../assets/icones/project.png'
 
 function Web() {
     return (
         <div className='web'> 
             <div className='web-option'>
                 <a href='#project'>
-                <i class="fi-rr-edit-alt option-icon"></i>Projects
+                <img src={project} alt='certificate-logo'/>Projects
                 </a>
             </div>
             <div className='web-option'>
                 <a href='#skills'>
-                <i class="fi-rr-laptop option-icon"> </i>Skills
+                <img src={skills} alt='certificate-logo'/>Skills
                 </a>
             </div>
             <div className='web-option'>
                 <a href='#work'>
-                <i class="fi-rr-briefcase option-icon" ></i>Work
+                <img src={work} alt='certificate-logo'/>Work
+                </a>
+            </div>
+            <div className='web-option'>
+                <a href='#education'>
+                <img src={book} alt='certificate-logo'/>Education
+                </a>
+            </div>
+            <div className='web-option'>
+                <a href='#certification'>
+                <img src={certificate} alt='certificate-logo'/>Certificates
                 </a>
             </div>
             <div className='web-option'>
